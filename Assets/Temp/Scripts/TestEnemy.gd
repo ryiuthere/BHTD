6f1b29f9b78e87ccf99760aff26cd4404c98c771
Ralston Yiu <ralstonyiu@yahoo.com>
2023-10-27 16:44:14 -0500

    Removed temporary items, moved icon to correct location

--- Assets/Temp/Scripts/TestEnemy.gd
+++
@@ -1,6 +0,0 @@
-extends entity
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var sprite := $Sprite2D as Sprite2D
-	sprite.modulate = Color.RED;
--- Assets/Temp/Scripts/TestEntity.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-class_name entity
-
-@export var moveSpeed = 200
-
-func _physics_process(_delta):
-	move_and_slide()
