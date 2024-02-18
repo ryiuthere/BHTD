b00714c80a191933273930a19973488ef128e131
ryiuthere <ralstonyiu@yahoo.com>
2024-02-18 14:32:32 -0600

    Started attack state

--- Assets/Scripts/Constants.gd
+++ Assets/Scripts/Constants.gd
@@ -1,6 +1,7 @@
 extends Node
 
 enum STATE_NAME {IDLE, CROUCH, WALK, RUN, JUMP, DOUBLEJUMP, AIR}
+enum ATTACK_STATE_NAME {L5, L2, L8, L6, M5, M2, M8, M6, H5, H2, H8, H6}
 enum INPUT {Jump, Light, Mid, Heavy, Surge, Burst, Parry}
 
 var WALK_RUN_SENSITIVITY := 10.0 # The lower, the easier to switch from idle/walking to running
@@ -10,4 +11,6 @@ var GRAVITY := 1600
 var FLOAT_DEADZONE := 0.02
 var CROUCH_ANGLE_MIN := PI / 4 - FLOAT_DEADZONE
 var CROUCH_ANGLE_MAX := 3 * PI / 4 + FLOAT_DEADZONE
+var UP_ANGLE_MIN := 5 * PI / 4 - FLOAT_DEADZONE
+var UP_ANGLE_MAX := 7 * PI / 4 + FLOAT_DEADZONE
 var FRAME_BUFFER := 3
